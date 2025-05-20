806c5ecab01667741a26ec8ee81630368fb2180a
Andrei-Dan Danila <121155901+andreidanila1@users.noreply.github.com>
2025-05-20 13:00:49 +0300

    Update build_mingw_pkg.sh

--- scripts/build_mingw_pkg.sh
+++ scripts/build_mingw_pkg.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
-set -o pipefail  # Prevent errors in a pipeline from being masked
+## Set STAGING
+USE_STAGING=OFF
+
 
 echo "Navigating to the package generator directory..."
-cd /home/docker/scopy/tools/packagegenerator
+cd /home/docker/scopy/scopy/tools/packagegenerator
 
 echo "Installing Python dependencies..."
 pip3 install -r requirements.txt
@@ -13,30 +14,29 @@ echo "Testing the package generator script..."
 python3 ./package_generator.py -h
 
 echo "Creating package directory for the repository..."
-mkdir -p /home/docker/scopy/packages/${GITHUB_REPOSITORY}
+mkdir -p /home/docker/scopy/scopy/packages/$GITHUB_REPOSITORY
 
 echo "Copying package files to the repository directory..."
-cp -r /home/docker/scopy/package/* /home/docker/scopy/packages/${GITHUB_REPOSITORY}/
+cp -r /home/docker/scopy/package/* /home/docker/scopy/scopy/packages/$GITHUB_REPOSITORY/
 
-echo "Navigating to the Scopy root directory..."
-cd /home/docker/scopy
+echo "Building scopy.."
+cd /home/docker/scopy/scopy
 
 echo "Creating and navigating to the build directory..."
 mkdir -p build && cd build
 
-echo "Running CMake with MinGW Makefiles..."
-cmake .. -G "MinGW Makefiles"
+source /home/docker/scopy/scopy/ci/windows/mingw_toolchain.sh $USE_STAGING
 
-echo "Building the project with MinGW Make..."
-mingw32-make -j$(nproc)
+$CMAKE .. $RC_COMPILER_OPT -DPYTHON_EXECUTABLE=$STAGING_DIR/bin/python3.exe -DENABLE_TESTING=OFF -DDISABLE_ALL_PACKAGES:BOOL=ON -DENABLE_PACKAGE_NEWPACKAGE:BOOL=ON
+$MAKE_BIN $JOBS
 
 echo "Copying the package directory to the artifact location..."
-cp -r /home/docker/scopy/packages/${GITHUB_REPOSITORY} /home/docker/artifact_x86_64 || echo "Package directory not found!"
+cp -r /home/docker/scopy/scopy/build/packages/$GITHUB_REPOSITORY /home/docker/artifact_x86_64 || echo "Package directory not found!"
 
 echo "Returning to the package generator directory..."
-cd /home/docker/scopy/tools/packagegenerator
+cd /home/docker/scopy/scopy/tools/packagegenerator
 
 echo "Running the package generator script to create the final artifact..."
-python3 ./package_generator.py -a --src=/home/docker/artifact_x86_64/${GITHUB_REPOSITORY} --dest=/home/docker/artifact_x86_64
+$STAGING_DIR/bin/python3.exe ./package_generator.py -a --src=/home/docker/artifact_x86_64/$GITHUB_REPOSITORY --dest=/home/docker/artifact_x86_64
 
 echo "Build and packaging process completed successfully!"

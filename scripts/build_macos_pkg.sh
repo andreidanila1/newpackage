d470bba283bf0c10f51e006d7b1dce18f108acc7
andreidanila1 <andrei.danila@analog.com>
2025-05-22 13:57:18 +0300

    Macos CI.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

---
+++ scripts/build_macos_pkg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -ex
+
+# Scopy path
+REPO_SRC=$(realpath scopy)
+source $REPO_SRC/ci/macOS/macos_config.sh
+
+build_scopy(){
+	echo "### Building Scopy"
+	ls -la $REPO_SRC
+	pushd $REPO_SRC
+
+	rm -rf $REPO_SRC/build
+	mkdir -p $REPO_SRC/build
+	cd $REPO_SRC/build
+	cmake \
+		-DCMAKE_LIBRARY_PATH="$STAGING_AREA_DEPS" \
+		-DCMAKE_INSTALL_PREFIX="$STAGING_AREA_DEPS" \
+		-DCMAKE_PREFIX_PATH="${STAGING_AREA_DEPS};${STAGING_AREA_DEPS}/lib/cmake;${STAGING_AREA_DEPS}/lib/pkgconfig;${STAGING_AREA_DEPS}/lib/cmake/gnuradio;${STAGING_AREA_DEPS}/lib/cmake/iio" \
+		-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+		-DCMAKE_VERBOSE_MAKEFILE=ON \
+		-DCMAKE_STAGING_PREFIX="$STAGING_AREA_DEPS" \
+		-DCMAKE_EXE_LINKER_FLAGS="-L${STAGING_AREA_DEPS}/lib" \
+		-DENABLE_TESTING=OFF \
+		../
+	CFLAGS=-I${STAGING_AREA_DEPS}/include LDFLAGS=-L${STAGING_AREA_DEPS}/lib make ${JOBS}
+	otool -l ./Scopy.app/Contents/MacOS/Scopy
+	otool -L ./Scopy.app/Contents/MacOS/Scopy
+	popd
+}
+
+build_scopy
